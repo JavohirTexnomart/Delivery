7bdd57cd5fc99fa46ca2313071f64e5f9366ce02
jmirzaev <jmirzaev@texnomart.uz>
2021-06-03 11:51:56 +0500

    ignore C.kt

--- app/src/main/java/mrj/example/deliverytexnomart/model/C.kt
+++ app/src/main/java/mrj/example/deliverytexnomart/model/C.kt
@@ -2,11 +2,14 @@ package mrj.example.deliverytexnomart.model
 
 
 /**
-* Created by JavohirAI
-*/
+ * Created by JavohirAI
+ */
 
 object C {
 
     val ORDER_KEY = "ORDER_KEY"
+    val url_1c = "http://192.168.0.150/"
+    var current_user = User()
+    val REQUEST_PHONE_CALL = 1
 
 }
