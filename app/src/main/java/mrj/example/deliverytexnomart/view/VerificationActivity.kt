06f1e707460b47c0e87927ef3f1e768fb57ca7af
jmirzaev <jmirzaev@texnomart.uz>
2021-06-04 17:01:17 +0500

    upgrade VerificationActivity.kt

--- app/src/main/java/mrj/example/deliverytexnomart/view/VerificationActivity.kt
+++ app/src/main/java/mrj/example/deliverytexnomart/view/VerificationActivity.kt
@@ -1,6 +1,8 @@
 package mrj.example.deliverytexnomart.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.view.MenuItem
 import mrj.example.deliverytexnomart.BaseActivity
 import mrj.example.deliverytexnomart.R
 import mrj.example.deliverytexnomart.databinding.VerificationActivityBinding
@@ -8,6 +10,7 @@ import mrj.example.deliverytexnomart.databinding.VerificationActivityBinding
 class VerificationActivity : BaseActivity(homeDislpayEnabled = true) {
 
     lateinit var binding: VerificationActivityBinding
+    var sendLetter = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,28 @@ class VerificationActivity : BaseActivity(homeDislpayEnabled = true) {
         setActionBar(binding.includeToolbar.myToolbar)
 
         binding.etxtVerificationCode.setHint(R.string.text_verification_code)
+
+        binding.btnConfirmVerification.setOnClickListener {
+            sendLetter = true
+            enableConfirmButton()
+            val handler = Handler()
+            handler.postDelayed({
+                sendLetter = false
+                enableConfirmButton()
+            }, 2000)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (sendLetter) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun enableConfirmButton() {
+        binding.btnConfirmVerification.isEnabled = !sendLetter
+        binding.txtSendAgain.isEnabled = !sendLetter
     }
 
 }
